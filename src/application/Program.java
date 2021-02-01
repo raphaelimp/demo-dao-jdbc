580ee1ea8d31b2a74050e9a9b9a1f67627a59d26
Raphael Imperio <imperioraphael@gmail.com>
2021-02-01 13:10:13 -0300

    findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,9 +12,16 @@ public class Program {
 		
 		SellerDao sd = DaoFactory.createSellerDao();
 		
+		System.out.println("/n === Find By Department ===");
 		Seller seller = sd.findById(3);
 		List<Seller> list = sd.findByDepartment(seller.getDepartment());
 		for(Seller s : list) {
+			System.out.println(s);
+		}
+		
+		System.out.println("=== Find All ===");
+		list = sd.findAll();
+		for (Seller s : list) {
 			System.out.println(s);
 		}
 		
